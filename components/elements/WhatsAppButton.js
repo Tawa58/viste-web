03225fe22898061ed663641f5056e85e7c3762c7
Tawanda Antonio <tawandaantonio@gmail.com>
2025-10-15 02:59:16 +0200

    Update WhatsApp button messages to reference VISTEM Academy instead of Elite School

--- components/elements/WhatsAppButton.js
+++ components/elements/WhatsAppButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const WhatsAppButton = () => {
   const phoneNumber = '+2630789984774'; // Updated phone number
-  const message = 'Hello! I would like to know more about Elite School for Kids.';
+  const message = 'Hello! I would like to know more about VISTEM Academy - Visionary Institute of Science and Technology.';
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
   return (
