135e95064f10c5bf55180e44839a247e8b6fb5d0
Tawanda Antonio <tawandaantonio@gmail.com>
2025-11-02 20:13:52 +0200

    Add Google site verification meta tag to HTML head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
+        {/* Google Site Verification */}
+        <meta name="google-site-verification" content="UKEOPHLGjyE7mfNCLvYuSPabwJs1ZgISYXmZ3KWG7uw" />
+        {/* ...existing meta tags... */}
       </Head>
       <body>
         <Main />
